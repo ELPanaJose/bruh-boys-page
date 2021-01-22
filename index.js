c123dd5d22a8aad109701d385f74124f63780e7d
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-01-21 20:14:03 -0600

    different style

--- index.js
+++ index.js
@@ -3,21 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.get("/people", (req, res) => {
-  res.sendFile(__dirname + "/static/people.html");
-});
-
-app.get("/about", (req, res) => {
   res.sendFile(__dirname + "/static/about.html");
 });
 
 app.get("/style1.css", (req, res) => {
   res.sendFile(__dirname + "/static/style1.css");
 });
-
 
 app.get("/style.css", (req, res) => {
   res.sendFile(__dirname + "/style.css");
